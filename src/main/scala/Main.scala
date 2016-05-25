ca722523058b16b8dc1bc857bdbdefd7adb85898
mikejlong60 <mjlong@functionicity.com>
2016-05-25 17:11:07 -0400

    Fixed a bug.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,27 +7,27 @@ object Main extends App {
 
   val cities = listOfLines.map(line => line.split("\\|")).map(line => CityDemographics(population = line(0), city = line(1), state = line(2), interstates = line(3).split(";").toSet)).toSet
   val chicago = cities.filter(_.city == "Chicago")
-  val endResult = degreesFrom(chicago, cities)// :+ Set(chicago)
+  val endResult = degreesFrom(chicago, cities) // :+ Set(chicago)
 
   val finalResult = endResult.flatten
-//  import scala.util.Sorting
-//  val pairs = Array(("a", 5, 2), ("c", 3, 1), ("b", 1, 3))
-//
-//  // sort by 2nd element
-//  Sorting.quickSort(finalResult)(Ordering[CityDemographics].on(x => ))
-//
-//  // sort by the 3rd element, then 1st
-//  Sorting.quickSort(pairs)(Ordering[(Int, String)].on(x => (x._3, x._1)))
-//  Sorting.quickSort(finalResult)(Ordering[(String)].on(x => (x._3, x._1)))
+  //  import scala.util.Sorting
+  //  val pairs = Array(("a", 5, 2), ("c", 3, 1), ("b", 1, 3))
+  //
+  //  // sort by 2nd element
+  //  Sorting.quickSort(finalResult)(Ordering[CityDemographics].on(x => ))
+  //
+  //  // sort by the 3rd element, then 1st
+  //  Sorting.quickSort(pairs)(Ordering[(Int, String)].on(x => (x._3, x._1)))
+  //  Sorting.quickSort(finalResult)(Ordering[(String)].on(x => (x._3, x._1)))
   println("===============")
   endResult.foreach({
-    level => {
-      println("length:"+level.size)
-      println(level)
-    }
+    level =>
+      {
+        println("length:" + level.size)
+        println(level)
+      }
   })
   println("++++++++++++++")
-
 
   def getDirectConnections(currentCities: Set[CityDemographics], nextLevelCandidates: Set[CityDemographics], degreeOfConnection: Int): Set[CityDemographics] = {
     @tailrec
@@ -36,31 +36,30 @@ object Main extends App {
         nextLevelCandidates.filter(candidateCity => candidateCity.interstates.intersect(city.interstates).nonEmpty)
 
       currentCities match {
-       case city :: Nil => {
-         accum ++ matchesForCity(city)
-       }
-       case city :: cities => {
-         getDirectConnections(cities, accum ++ matchesForCity(city))
-       }
-       case Nil => accum
-     }
-   }
+        case city :: Nil => accum ++ matchesForCity(city)
+        case city :: cities => getDirectConnections(cities, accum ++ matchesForCity(city))
+        case Nil => accum
+      }
+    }
     getDirectConnections(currentCities.toList, Set.empty[CityDemographics])
   }
 
   def degreesFrom(startingLevel: Set[CityDemographics], candidates: Set[CityDemographics]): Vector[Set[CityDemographics]] = {
+    def addLevels(linkedLevels: Vector[Set[CityDemographics]]) =
+      linkedLevels.foldLeft(Vector.empty[Set[CityDemographics]])((allCities, cities) => allCities :+ cities.map(city => city.copy(degreeOfConnection = allCities.length)))
+
     @tailrec
     def degreesFrom(currentLevel: Set[CityDemographics], candidates: Set[CityDemographics], accum: Vector[Set[CityDemographics]], degreeOfConnection: Int): Vector[Set[CityDemographics]] = {
       val directConnections = getDirectConnections(currentLevel, candidates -- currentLevel, degreeOfConnection)
-      if (directConnections.isEmpty)
-        accum
-      else
+      if (directConnections.isEmpty) {
+        addLevels(accum).+:(candidates -- currentLevel).reverse
+      } else
         degreesFrom(directConnections, candidates -- currentLevel, accum :+ directConnections, degreeOfConnection + 1)
     }
-    degreesFrom(startingLevel, candidates -- startingLevel, Vector.empty[Set[CityDemographics]], 0)
+    degreesFrom(startingLevel, candidates -- startingLevel, Vector(startingLevel), 0)
   }
 }
 
-case class CityDemographics(degreeOfConnection: Int = -1, city: String, state: String, interstates: Set[String], population: String)// extends Ordered[CityDemographics] {
+case class CityDemographics(degreeOfConnection: Int = -1, city: String, state: String, interstates: Set[String], population: String) // extends Ordered[CityDemographics] {
 //  def compare(that: CityDemographics) =  this.degreesOfConnection - that.degreesOfConnection && this.state > that.state
 //}
